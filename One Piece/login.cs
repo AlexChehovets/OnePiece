167df4305b43b14e5eba60aa03edd4c3e50c1f9e
justJulit <rabiychukj@gmail.com>
2021-10-10 22:04:29 +0300

    create db in phpMyAdmin and binding DB to project

---
+++ One Piece/login.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace One_Piece
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void butlog_Click(object sender, EventArgs e)
+        {
+            String loginUser = logfield.Text;
+            String passUser = passfield.Text;
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login`= @ul AND `password`= @up",  db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@up", MySqlDbType.VarChar).Value = passUser;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Так");
+            }
+            else
+                MessageBox.Show("ні");
+        }
+    }
+}
